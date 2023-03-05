93ce0b9bab2a085e47c719a12e55de807cac5b34
Filip Mikic <filip187m@gmail.com>
2023-03-05 10:47:16 +0100

    style TilePicker

--- src/components/MapBuilder/MapBuilder.tsx
+++ src/components/MapBuilder/MapBuilder.tsx
@@ -1,11 +1,17 @@
 import styles from './MapBuilder.module.scss';
 import TilePicker from './TilePicker/TilePicker';
+import { useState } from 'react';
+import { TileSign } from '../../types/TileSign';
 
 const MapBuilder = () => {
+  const [selectedTile, setSelectedTile] = useState<TileSign>('_');
   return (
     <div className={styles.container}>
       <aside>
-        <TilePicker />
+        <TilePicker
+          selectedTile={selectedTile}
+          onTileSelect={setSelectedTile}
+        />
       </aside>
     </div>
   );
--- src/components/MapBuilder/TilePicker/TilePicker.tsx
+++ src/components/MapBuilder/TilePicker/TilePicker.tsx
@@ -4,24 +4,58 @@ import Destination from '../../map-tiles/Destination/Destination';
 import Wall from '../../map-tiles/Wall/Wall';
 import Player from '../../map-tiles/Player/Player';
 import Box from '../../map-tiles/Box/Box';
+import { TileSign } from '../../../types/TileSign';
+import classNames from 'classnames';
 
-const TilePicker = () => {
+type Props = {
+  selectedTile: TileSign;
+  onTileSelect: (sign: TileSign) => unknown;
+};
+
+const TilePicker = ({ selectedTile, onTileSelect }: Props) => {
+  function getItemClassName(tile: TileSign) {
+    return classNames(
+      styles.item,
+      tile === selectedTile && styles['item--selected'],
+    );
+  }
+
   return (
     <div className={styles.container}>
       <ul className={styles.items}>
-        <li className={styles.item} title="Floor">
+        <li
+          className={getItemClassName('_')}
+          onClick={() => onTileSelect('_')}
+          title="Floor"
+        >
           <Floor />
         </li>
-        <li className={styles.item} title="Wall">
+        <li
+          className={getItemClassName('#')}
+          onClick={() => onTileSelect('#')}
+          title="Wall"
+        >
           <Wall />
         </li>
-        <li className={styles.item} title="Destination">
+        <li
+          className={getItemClassName('*')}
+          onClick={() => onTileSelect('*')}
+          title="Destination"
+        >
           <Destination />
         </li>
-        <li className={styles.item} title="Box">
+        <li
+          className={getItemClassName('B')}
+          onClick={() => onTileSelect('B')}
+          title="Box"
+        >
           <Box />
         </li>
-        <li className={styles.item} title="Player">
+        <li
+          className={getItemClassName('@')}
+          onClick={() => onTileSelect('@')}
+          title="Player"
+        >
           <Player />
         </li>
       </ul>
